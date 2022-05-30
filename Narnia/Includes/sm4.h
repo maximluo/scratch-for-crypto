c4e5feeee9046a8a439ebd5521a7974c3e26f1fd
Maxim <maximluo@gmail.com>
2022-05-31 00:37:59 +0800

    [*] complete round key generation

--- Narnia/Includes/sm4.h
+++ Narnia/Includes/sm4.h
@@ -16,6 +16,9 @@ private:
 
 	void GetUserKey(void* dest, const byte* userKey, int keyLength);
 
+	word32 SM4_G(word32 x);
+	word32 SM4_H(word32 x);
+
 	word32 m_rkeys[SM4_NUM_ROUNDS];
 
 	word32 m_wspace[5];
